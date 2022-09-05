2e13073909760f6d9338e8ef6184a7cb0aaa1003
KIRAN GOPALAKRISHNAN <112387719+KiranGk4@users.noreply.github.com>
2022-09-05 12:29:23 +0530

    Add files via upload

--- JAVA/This_Example4.java
+++ JAVA/This_Example4.java
@@ -1,16 +1,16 @@
 package com.ust;
 
-public class This_Example4 {
+class This_Example41 {
 
 	int val_a, val_b;
 	
-	This_Example4()		// default constructors
+	This_Example41()		// default constructors
 	{
 		val_a = 10;
 		val_b = 20;
 	}
 	
-	This_Example4 get()
+	This_Example41 get()
 	{
 		return this;
 	}
@@ -19,9 +19,13 @@ public class This_Example4 {
 	{
 		System.out.println("val_a = "+val_a+" val_b = "+val_b);
 	}
+}
+	
+public class This_Example4
+{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		This_Example4 object = new This_Example4();
+		This_Example41 object = new This_Example41();
 		object.get().display();
 	}
 

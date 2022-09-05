ad2ead0a641f4eeb742d0b463d6c3bb2d455b2ee
KIRAN GOPALAKRISHNAN <112387719+KiranGk4@users.noreply.github.com>
2022-09-05 10:07:11 +0530

    Update Final_Demo.java

--- JAVA/Final_Demo.java
+++ JAVA/Final_Demo.java
@@ -2,10 +2,10 @@ package com.ust;
 
 public class Final_Demo {
 	
-	final int daysInWeek;
+	final int DAYS_IN_WEEK;
 	Final_Demo()
 	{
-		daysInWeek = 7;
+		DAYS_IN_WEEK = 7;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

de8d179ee5c14caad2bd283725c47caf0b112106
Kiran GK <Kiran.Gopalakrishnan@ust.com>
2022-10-31 17:32:15 +0530

    changes made

--- JAVA/Final_Demo3.java
+++ JAVA/Final_Demo3.java
@@ -1,18 +1,39 @@
 package com.ust;
+import java.lang.System;
+/**
+ * The type Final demo 1.
+ */
 class FinalDemo1
 {
+	/**
+	 * Display.
+	 */
 	public final void display()
 	{
 		System.out.println("This is a final method.");
 	}
 }
+
+/**
+ * The type Final demo 3.
+ */
 public class Final_Demo3 extends FinalDemo1
 {
-	public final void display1()
+	/**
+	 * Display 1.
+	 */
+	public final int display1()
 	{
+
 		System.out.println("The final method is overridden.");
+		return 0;
 	}
 
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
 	public static void main(String[] args)
 	{
 		Final_Demo3 obj = new Final_Demo3();
